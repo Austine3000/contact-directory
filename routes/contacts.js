475ff8f2c58148dcf54e5c07aeac8e19c434454b
Austine3000 <diribe3000@gmail.comgit config --global user.email diribe3000@gmail.com>
2018-12-10 16:19:42 +0100

    Added update method for contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -57,4 +57,46 @@ router.get('/', function(req, res, next) {
         })
 })
 
+router.put('/:id/update', function(req, res, next) {
+    req.sanitize('id').escape();
+    req.sanitize('id').trim();
+
+    req.checkBody('first_name', 'First name must be specified.').notEmpty(); 
+    req.checkBody('last_name', 'Last name must be specified.').notEmpty();
+    req.checkBody('phone_number', 'Phone number must be specified.').notEmpty(); 
+    req.checkBody('category', 'Category must be specified.').notEmpty();
+
+    req.sanitize('first_name').escape();
+    req.sanitize('last_name').escape();
+    req.sanitize('phone_number').escape();
+    req.sanitize('category').escape();
+
+    req.sanitize('first_name').trim();     
+    req.sanitize('last_name').trim();
+    req.sanitize('phone_number').trim();     
+    req.sanitize('category').trim();
+
+
+    var errors = req.validationErrors();
+    
+    var contact = new Contact(
+      { first_name: req.body.first_name, 
+        last_name: req.body.last_name, 
+        phone_number: req.body.phone_number,
+        category: req.body.category,
+        _id: req.params.id
+       });
+       
+    if (errors) {
+        res.json({contact: Contact, errors: errors });
+        return;
+    } 
+    else {
+        Contact.findByIdAndUpdate(req.params.id, contact, {}, function (err) {
+            if (err) { return next(err); }
+            res.json(contact);
+        });
+    }
+})
+
 module.exports = router;
